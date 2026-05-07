7a44c64a6e6ab62be02272c8d1de5b09b307204f
NMI <nicolasmintjens@gmail.com>
2026-05-07 20:19:55 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,34 @@
-self.addEventListener("install",e=>{
-console.log("Service Worker actief");
+const CACHE_NAME = "app-cache-v2";
+
+// Install
+self.addEventListener("install", (event) => {
+  console.log("SW geïnstalleerd");
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  console.log("SW geactiveerd");
+
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key.startsWith("app-cache-") && key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request, { credentials: "same-origin" })
+      .catch(() => caches.match(event.request))
+  );
 });
